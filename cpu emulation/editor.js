55ed94c26995cee740c2f58100f071c14aeada94
blazjerman <blaz.jerman5@gmail.com>
2023-07-05 20:53:04 +0200

    Added proper editor

--- cpu emulation/editor.js
+++ cpu emulation/editor.js
@@ -1,9 +1,7 @@
 const codeTextarea = document.getElementById("code");
 const overCodeTextarea = document.getElementById("overCode");
 
-const spaceRegex = /^[\s\S]*\S[\s\S]*$/;
 const commentRegex = /(\/\/.*)/g;
-const nextLineRegex = /\n/g;
 
 codeTextarea.addEventListener("input", function () {
   updateLineNumbers();
@@ -16,23 +14,18 @@ function updateLineNumbers() {
   for (let i = 0; i < codeTextarea.value.split("\n").length; i++)numbers = numbers + (i + 1) + "<br>" ;
   document.querySelector(".lines").innerHTML = numbers;
 
-  let text = codeTextarea.value;
-  
-  text = text.replace(commentRegex, '<span style="color: green" class="special-characters">$1</span>');
-  text = text.replace(nextLineRegex, "<br>");
-
-  //overCodeTextarea.innerHTML = text;
-
 }
 
 
 function getFilteredCodeText(){
 
-  let text = codeTextarea.value;
+  let textArray = codeTextarea.value.split("\n");
 
-  text = text.replace(commentRegex, '');
-  
-  return text.split("\n").filter(word => spaceRegex.test(word));
+  for (let i = 0; i < textArray.length; i++){
+    textArray[i] = textArray[i].replace(commentRegex, '').trimEnd().trimStart();
+  }
+
+  return textArray;
 }
 
 
